f1574f7d91c5848e489b1c165a2309af8e983340
nevenisnoob <lj988511@gmail.com>
2015-03-09 15:25:11 +0900

    Initial Commit

---
+++ FirstSingleView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstSingleView
+//
+//  Created by huang liangjin on 2015/03/09.
+//  Copyright (c) 2015年 huang liangjin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FirstSingleView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FirstSingleView
+//
+//  Created by huang liangjin on 2015/03/09.
+//  Copyright (c) 2015年 huang liangjin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
